01ab79a6dc5ff48df5cecd37072120b9e11b13d3
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-11-26 14:54:16 +0600

    feat: initialize data list with useEffect

--- dataFilter/src/components/DataFilter.js
+++ dataFilter/src/components/DataFilter.js
@@ -4,6 +4,26 @@ const DataFilter = () => {
     const [data, setData] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const sampleData = [
+                { id: 1, name: 'Altynai' },
+                { id: 2, name: 'Zhanetta' },
+                { id: 3, name: 'Aisuluu' },
+                { id: 4, name: 'Roza' },
+                { id: 5, name: 'Sasha' },
+                { id: 6, name: 'Bermet' },
+                { id: 7, name: 'Dilnaz' },
+                { id: 8, name: 'Aikan' },
+                { id: 9, name: 'Anjelika' },
+                { id: 10, name: 'Aijan' },
+            ];
+            setData(sampleData);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h1>Data filter</h1>
