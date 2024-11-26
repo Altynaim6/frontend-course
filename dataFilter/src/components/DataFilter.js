cb405dd63f9a881ff98e302ef20e8be497d4b064
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-11-26 14:48:09 +0600

    feat: add state for data and search query

---
+++ dataFilter/src/components/DataFilter.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const DataFilter = () => {
+    const [data, setData] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    return (
+        <div>
+            <h1>Data filter</h1>
+        </div>
+    );
+};
+
+export default DataFilter;
