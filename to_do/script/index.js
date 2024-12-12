eb1b47c887c9ee5a4337e6296ce00deb8c1f29e2
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-12-12 21:21:56 +0600

    feat: add a feature to edit tasks

--- to_do/script/index.js
+++ to_do/script/index.js
@@ -11,6 +11,13 @@ imgDone = `
 </svg>
 `;
 
+imgEdit = `
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>
+`;
+
 imgTrash = `
 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
@@ -45,10 +52,12 @@ const renderToDos = () => {
         const title = document.createElement('h2');
         const btnsWrapper = document.createElement('div');
         const done = document.createElement('button');
+        const edit = document.createElement('button');
         const trash = document.createElement('button');
 
         title.textContent = el.name;
         done.innerHTML = imgDone;
+        edit.innerHTML = imgEdit;
         trash.innerHTML = imgTrash;
 
         done.addEventListener('click', () => {
@@ -62,6 +71,17 @@ const renderToDos = () => {
             card.classList.remove('done');
         }
 
+        edit.addEventListener('click', () => {
+            const answer = confirm('Are you sure you want to change the name?');
+            if (answer) {
+                const newName = prompt('Enter the new name');
+                if (newName && newName.trim().length) {
+                    el.name = newName;
+                    renderToDos();
+                }
+            }
+        });
+
         trash.addEventListener('click', () => {
             const answer = confirm('Are you sure you want to delete this task?');
             if (answer) {
@@ -70,7 +90,7 @@ const renderToDos = () => {
             }
         });
 
-        btnsWrapper.append(done, trash);
+        btnsWrapper.append(done, edit, trash);
         card.append(title, btnsWrapper);
         output.append(card);
     });
